a940cfdb0f3ddbc3cc0f028bf8358f8781e12d74
HAYLTON ILONGA <eltonilonga@gmail.com>
2025-09-25 22:03:20 -0400

    ADD TEST PAGE: v3.0 deployment verification

---
+++ OneDrive/Bureau/waksoccer/apps/web/app/test/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function TestPage() {
+  return (
+    <div>
+      <h1>DEPLOYMENT TEST v3.0 - 2024-12-22 16:50:00</h1>
+      <button onClick={() => alert('CLICK WORKS!')}>TEST BUTTON</button>
+      <p>If you can see this and the button works, deployment is working!</p>
+    </div>
+  );
+}
